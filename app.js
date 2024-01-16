65b84f53521391ca211c1ad55b85a27602704335
Samuvel6826 <samuvel6826@gmail.com>
2024-01-16 08:52:53 +0530

    Nodejs- Day -1 : Nodejs

---
+++ app.js
@@ -0,0 +1,98 @@
+const createError = require('http-errors');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const moment = require('moment')
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/getTextFiles', (req, res) => {
+
+  // Specify the folder path where the text files are stored
+  const folderPath = path.join(__dirname, 'files');
+
+  // Check if the folder exists
+  if (!fs.existsSync(folderPath)) {
+      return res.status(404).json({ error: 'Folder not found' });
+  }
+
+  // Read all files in the folder
+  fs.readdir(folderPath, (err, files) => {
+      if (err) {
+          return res.status(500).json({ error: 'Error reading folder' });
+      }
+
+      // Filter only text files (files with the .txt extension)
+      const textFiles = files.filter(file => path.extname(file) === '.txt');
+
+      res.json({ textFiles });
+  });
+
+});
+
+app.post('/createTxtFile', (req, res) => {
+
+  const currentTimestamp = moment().format('HH:mm:ss');
+  const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  // Specify the folder path where the file should be created
+  const folderPath = path.join(__dirname, 'files');
+
+  // Create the folder if it doesn't exist
+  if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+  }
+
+  // Specify the file path
+  const filePath = path.join(folderPath, `${currentDateTime}.txt`);
+
+  // Write content to the file
+  fs.writeFile(filePath, `Current Timestamp : ${currentTimestamp}`, (err) => {
+      if (err) {
+          return res.status(500).json({ error: 'Error creating file' });
+      }
+      else
+      {
+          return res.status(200).json({ message: 'File created successfully' });
+      }
+  });
+
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
